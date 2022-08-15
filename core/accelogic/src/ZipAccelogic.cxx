ab387973d1dbd6ab59c7c8170f17b7492095f425
Gene Van Buren <gene@bnl.gov>
2022-08-15 15:55:23 -0500

    Blast: correct out_size recording
    Must add 2 before bit-shifting, as 255+2 will affect second byte, etc.

--- core/accelogic/src/ZipAccelogic.cxx
+++ core/accelogic/src/ZipAccelogic.cxx
@@ -188,9 +188,10 @@ void R__zipBLAST(unsigned char *cxlevels, int *srcsize, char *src, int *tgtsize,
       // tgt[2] is set for each target buffer above
 
       // Include the 2 extra header byte into the out size.
-      tgt[3] = (char)((out_sizes[tgt_idx] + 2) & 0xff);
-      tgt[4] = (char)((out_sizes[tgt_idx] >> 8) & 0xff);
-      tgt[5] = (char)((out_sizes[tgt_idx] >> 16) & 0xff);
+      const size_t out_size = out_sizes[tgt_idx] + 2;
+      tgt[3] = (char)((out_size) & 0xff);
+      tgt[4] = (char)((out_size >> 8) & 0xff);
+      tgt[5] = (char)((out_size >> 16) & 0xff);
 
       tgt[6] = (char)(in_size & 0xff);         /* decompressed size */
       tgt[7] = (char)((in_size >> 8) & 0xff);
